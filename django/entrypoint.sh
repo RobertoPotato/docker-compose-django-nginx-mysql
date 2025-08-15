9c9efd475b1646026dbc104f885fc40ca6ee34ca
RobertoPotato <robertwabuti@gmail.com>
2025-08-15 11:34:33 +0300

    Changes made here wait for db to be ready before attempting to connect

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+echo "Waiting for db..."
+while ! nc -z db 3306; do
+  sleep 1
+done
+echo "Database ready."
+
 echo "Running migrations..."
 python manage.py migrate --noinput
 
