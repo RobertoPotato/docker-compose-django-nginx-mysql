89e35628d2e973faa956bdedc58efd5e40fb483e
RobertoPotato <robertwabuti@gmail.com>
2025-08-15 10:19:33 +0300

    Added support for nginx to serve static/media files

--- django/core/settings.py
+++ django/core/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = '/vol/static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/vol/media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
